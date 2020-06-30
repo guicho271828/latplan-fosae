e4a18c200e5ca7659e8ba019ea1112ef6d90f0a3
Masataro Asai <guicho2.71828@gmail.com>
2020-06-30 09:31:30 -0400

    download data from IBM/photorealistic-blocksworld

--- setup-dataset.sh
+++ setup-dataset.sh
@@ -7,6 +7,7 @@ set -e
 trap exit SIGINT
 
 ulimit -v 16000000000
+export SHELL=/bin/bash
 
 proj=$(date +%Y%m%d%H%M)setup
 common="jbsub -mem 64g -cores 1 -queue x86_1h -proj $proj"
@@ -26,3 +27,19 @@ $common PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 3 3 50000
 $common PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 4 4 50000
 $common PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 9 3 50000
 $common PYTHONUNBUFFERED=1 ./setup-dataset.py hanoi 4 8 50000
+
+download-and-extract (){
+    wget https://github.com/IBM/photorealistic-blocksworld/releases/download/$1/$1.npz -O latplan/puzzles/$1.npz
+    wget https://github.com/IBM/photorealistic-blocksworld/releases/download/$1/$1-init.json -O latplan/puzzles/$1-init.json
+    wget https://github.com/IBM/photorealistic-blocksworld/releases/download/$1/$1-stat.json -O latplan/puzzles/$1-stat.json
+}
+
+export -f download-and-extract
+
+$common PYTHONUNBUFFERED=1 download-and-extract blocks-5-3
+$common PYTHONUNBUFFERED=1 download-and-extract blocks-4-4
+$common PYTHONUNBUFFERED=1 download-and-extract blocks-3-7
+$common PYTHONUNBUFFERED=1 download-and-extract blocks-3-6
+$common PYTHONUNBUFFERED=1 download-and-extract blocks-3-5
+$common PYTHONUNBUFFERED=1 download-and-extract blocks-3-4
+$common PYTHONUNBUFFERED=1 download-and-extract blocks-3-3
